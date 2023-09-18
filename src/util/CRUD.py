b19efe69f5f45b8c74f91b5b41edd49914300c2e
hector <hectorrrivera1010@gmail.com>
2023-09-17 22:23:58 -0600

    commit

---
+++ src/util/CRUD.py
@@ -0,0 +1,23 @@
+"""
+        title: "Feynman lectures on physics",
+        description: "high quality up-to-date copy of Feynman's legendary lectures totallly free.",
+        url: "https://www.feynmanlectures.caltech.edu/",
+        img: './math2.png',
+        type: TYPES[1],
+        topic: TOPICS[0]
+"""
+
+def createEntry(title, description, url, img, type, topic):
+    """
+    this function create an entry for the API and print in console 
+    the result.
+    """
+    
+    print(
+        """
+        {
+            test: uno dos
+        }
+        """)
+    
+    
