c2b86f5ace99e529976c26f21a281e5b8c6a632e
hector <hectorrrivera1010@gmail.com>
2023-09-20 20:53:47 -0600

    [update] CRUD.pyu

--- src/util/CRUD.py
+++ src/util/CRUD.py
@@ -7,17 +7,20 @@
         topic: TOPICS[0]
 """
 
-def createEntry(title, description, url, img, type, topic):
+def createEntry(title, description, url, img, typeOfEntry, topic):
     """
     this function create an entry for the API and print in console 
     the result.
     """
     
-    print(
-        """
-        {
-            test: uno dos
-        }
-        """)
+    print("{")
+    print(f'title: {title},')
+    print(f'description: {description},')
+    print(f'url: {url},')
+    print(f'img: {img},')
+    print(f'type: {typeOfEntry},')
+    print(f'topic: {topic}')
+    print("}")
+
     
-    
+createEntry("Feynman lectures on physics", "high quality up-to-date copy of Feynman's legendary lectures totallly free.", "https://www.feynmanlectures.caltech.edu/", './math2.png', "1","2")
